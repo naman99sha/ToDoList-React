ee322729ea44604ae98c555893d1a217fdbac2e4
Naman Sharma <naman99sha@gmail.com>
2023-05-17 14:15:09 +0530

    Single component project

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,22 +1,56 @@
+import { useState } from "react"
 import "./styles.css"
 export default function App(){
+  const[newItem, setItem] = useState("");
+  const[itemList, setItemList] = useState([]);
+
+  function addItem(event){
+    event.preventDefault();
+    setItemList(prevItem => {
+      return [...itemList, {
+        id: crypto.randomUUID(), title: newItem, completed: false
+      }];
+    });
+    setItem("");
+  }
+
+  function handleComplete(id, checked){
+    setItemList(prevItem => {
+      return prevItem.map(itemObj => {
+        if (itemObj.id === id){
+          itemObj.completed = checked;
+        }
+        return itemObj
+      })
+    })
+  }
+
+  function handleDelete(id){
+    setItemList(prevItem => {
+      return prevItem.filter(itemObj => itemObj.id !== id)
+    })
+  }
+
   return (
   <>
   <form className="new-item-form">
     <div className="form-row">
       <label htmlFor="item">New Item</label>
-      <input type="text" id="item"></input>
-      <button className="btn">Add</button>
+      <input type="text" id="item" onChange={e => setItem(e.target.value)} value={newItem}></input>
+      <button className="btn" onClick={addItem}>Add</button>
     </div>
   </form>
   <h1 className="header">ToDo List</h1>
   <ul className="list">
-    <li>
+    {itemList.length === 0 && "No Todo Items"}
+    {itemList.map((itemObj) => (
+      <li key={itemObj.id}>
       <label>
-        <input type="checkbox"></input> Item 1
+        <input type="checkbox" checked={itemObj.completed} onChange={e => handleComplete(itemObj.id, e.target.checked)}></input> {itemObj.title}
       </label>
-      <button className="btn btn-danger">Delete</button>
+      <button className="btn btn-danger" onClick={e => handleDelete(itemObj.id)}>Delete</button>
     </li>
+    ))}
   </ul>
   </>
   )
